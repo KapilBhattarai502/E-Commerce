ad7ebe97a77871a45f073dbe90b04ad1b3913da7
Kapil Bhattarai <kapilbhattarai502@gmail.com>
2024-07-17 22:12:43 +0545

    starting my new project

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navigation from './customer/components/Navigation/Navigation'
+
+
+const App = () => {
+  return (
+    <>
+    <div>
+    <Navigation/>
+
+    </div>
+    </>
+  )
+}
+
+export default App
